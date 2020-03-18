b964ab4110d9a5871ae8863fbe9c25f4d739651e
Muhammad Talha <talhahadees786@gmail.com>
2020-03-19 00:04:37 +0500

    Add basic files and add basic packages

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>Hello Private Chat App</div>
+  )
+}
+export default App
